4701c99b2f760798fac7fa5cc997501883582073
Valentyn Ziomenko <ziomenkow@gmail.com>
2026-05-28 20:06:00 +0200

    add loader ErorrMessage components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import SearchBar from './SearchBar';
-import { useState } from 'react';
+import { use, useState } from 'react';
 import { Toaster, toast } from 'react-hot-toast';
 import fetchMovies from './services/movieService';
 import { type Movie } from './types/movie';
 import MovieGrid from './MovieGrid';
 import MovieModal from './MovieModal';
+import Loader from './Loader';
+import ErrorMessage from './ErrorMessage';
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [hasSearched, setHasSearched] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   async function handleSearch(query: string) {
     try {
+      setIsLoading(true);
+      setIsError(false);
       const response = await fetchMovies(query);
       setMovies(response.data.results);
+      setHasSearched(true);
     } catch {
       toast.error('Ошибка загрузки фильмов');
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -30,7 +41,6 @@ export default function App() {
     setIsModalOpen(true);
   }
 
-  // В JSX:
   <MovieGrid movie={movies} onSelect={handleSelect} />;
   {
     isModalOpen && selectedMovie && (
@@ -43,9 +53,15 @@ export default function App() {
       <Toaster />
       <SearchBar onSubmit={handleSearch} />
 
-      {movies.length > 0 && (
-        <MovieGrid movie={movies} onSelect={handleSelect} />
-      )}
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+
+      {hasSearched &&
+        (movies.length > 0 ? (
+          <MovieGrid movie={movies} onSelect={handleSelect} />
+        ) : (
+          toast.error('No movies found for your request.')
+        ))}
       {isModalOpen && selectedMovie && (
         <MovieModal onClose={closeModal} movie={selectedMovie} />
       )}
